5c489c925f55f83852e9034335fb3cb01d44b9a9
dhanushkumar <dhanushkumaramk@gmail.com>
2025-09-03 21:17:34 +0530

    modified

---
+++ PYTHON-BASIC/DataTypes.py
@@ -0,0 +1,48 @@
+# integer variable.
+a=100
+print("The type of variable having value", a, " is ", type(a))
+
+# float variable.
+c=20.345
+print("The type of variable having value", c, " is ", type(c))
+
+# complex variable.
+d=10+3j
+print("The type of variable having value", d, " is ", type(d))
+
+
+print("===============================")
+str = 'Hello World!'
+
+print (str)          # Prints complete string
+print (str[0])       # Prints first character of the string
+print (str[2:5])     # Prints characters starting from 3rd to 5th
+print (str[2:])      # Prints string starting from 3rd character
+print (str * 2)      # Prints string two times
+print (str + "TEST") # Prints concatenated string
+
+print("===============================")
+
+print ("Type casting.....")
+
+a = int(1)     # a will be 1
+b = int(2.2)   # b will be 2
+c = int("3")   # c will be 3
+
+print (a)
+print (b)
+print (c)
+
+print("====================")
+
+# Literals
+
+# Using Octal notation
+x = 0O34
+print (x, type(x))
+# Using Hexadecimal notation
+x = 0X1c
+print (x, type(x))
+
+
+
