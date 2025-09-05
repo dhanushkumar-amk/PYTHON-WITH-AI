ca768d72de21c7945a19adff73a1adc4c9ee2f8e
dhanushkumar <dhanushkumaramk@gmail.com>
2025-09-05 18:33:06 +0530

    modified

--- PYTHON-BASIC/classExample.py
+++ PYTHON-BASIC/classExample.py
@@ -1,8 +1,25 @@
 class Student:
-    def say_hello(self, name):
-        print("Hello World " + name)
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def say_hello(self):
+        print(f"Hello {self.name} and I am {self.age} years old.")
 
 
-student = Student()
-student.say_hello("dhanushkumar");
+student = Student("dhanushkumar", 21)
+student.say_hello()
 
+
+class StudentDetails:
+    def __init__(self, rollNO, className):
+        self.rollNO = rollNO
+        self.className = className
+
+
+    def getStudentInfo(self):
+        print(f"student roll no: {self.rollNO} and class name: {self.className}")
+
+
+studentDetails = StudentDetails(rollNO=21, className="Python")
+studentDetails.getStudentInfo()
